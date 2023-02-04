d5f447ed13bfd659ad23e30dee065bb892cf4cf1
NaveenKumar Namachivayam ⚡ <catch.nkn@gmail.com>
2023-02-04 13:30:53 -0500

    Add: exception handling
    Signed-off-by: NaveenKumar Namachivayam ⚡ <catch.nkn@gmail.com>

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 import openai
+import os
 
 app = Flask(__name__)
 
@@ -23,32 +24,39 @@ def features():
     return render_template("features.html")
 
 
-@app.route("/upload", methods=['POST'])
+@app.route('/upload', methods=['POST'])
 def askgpt_upload():
     """
     ask GPT
     :return:    analyzed response from GPT
     """
-    openai.api_key = ""
-    file = request.files['file']
-    print(file)
-
-    contents = file.read().decode("utf-8")
     try:
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=f"""
-            Please analyse this performance test results: \n {contents}
-            """,
-            temperature=0,
-            max_tokens=100,
-            top_p=1.0,
-            frequency_penalty=0.0,
-            presence_penalty=0.0
-        )
-        return render_template("analysis_response.html", response=response['choices'][0]['text'])
-    except Exception as e:
-        return e
+        openai.api_key = os.environ['OPENAI_API_KEY']
+    except KeyError:
+        return render_template("analysis_response.html", response="API key not set.")
+
+    if request.method == 'POST':
+        if request.files:
+            file = request.files['file']
+            contents = file.read().decode("utf-8")
+
+            try:
+                response = openai.Completion.create(
+                    model="text-davinci-003",
+                    prompt=f"""
+                    Please analyse this performance test results: \n {contents}
+                    """,
+                    temperature=0,
+                    max_tokens=100,
+                    top_p=1.0,
+                    frequency_penalty=0.0,
+                    presence_penalty=0.0
+                )
+                return render_template("analysis_response.html", response=response['choices'][0]['text'])
+            except Exception as e:
+                return e
+        else:
+            return render_template('analysis_response.html', response="Upload a valid file")
 
 
 if __name__ == '__main__':
