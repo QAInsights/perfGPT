2272f942776225e7730e66a2ea33a9db2681f249
NaveenKumar Namachivayam ⚡ <catch.nkn@gmail.com>
2023-02-07 18:59:42 -0500

    Add: multiple prompts
    Signed-off-by: NaveenKumar Namachivayam ⚡ <catch.nkn@gmail.com>

--- application.py
+++ application.py
@@ -44,11 +44,11 @@ def beautify_response(text):
     numbers = re.finditer(pattern, text)
     offset = 0
     for match in numbers:
-        num = text[match.start()+offset:match.end()+offset]
-        first_half, second_half = text[:match.start()+offset], text[match.end()+offset:]
+        num = text[match.start() + offset:match.end() + offset]
+        first_half, second_half = text[:match.start() + offset], text[match.end() + offset:]
         text = f'{first_half}<span class="fw-bold">{num}</span>{second_half}'
         offset += 29  # number of chars added by the <span> tags
-    
+
     return text
 
 
@@ -58,6 +58,13 @@ def askgpt_upload():
     ask GPT
     :return:    analyzed response from GPT
     """
+    # Below prompts dict has the results title and the prompt for GPT to process
+    prompts = {
+        "High level Summary": "Act like a performance engineer. Please analyse this performance test results and give "
+                              "me a high level summary.",
+        "Detailed Summary": "Act like a performance engineer and write a detailed summary from this raw performance "
+                            "results."
+    }
     try:
         openai.api_key = os.environ['OPENAI_API_KEY']
     except KeyError:
@@ -78,23 +85,27 @@ def askgpt_upload():
                 return render_template('analysis_response.html', response="Cannot read file data. Please make sure "
                                                                           "the file is not empty and is in one of the"
                                                                           " supported formats.")
-            
+
             if contents.memory_usage().sum() > constants.FILE_SIZE:
                 return render_template('analysis_response.html', response="File size too large.")
             try:
-                response = openai.Completion.create(
-                    model=constants.model,
-                    prompt=f"""
-                    Please analyse this performance test results: \n {contents}
-                    """,
-                    temperature=constants.temperature,
-                    max_tokens=constants.max_tokens,
-                    top_p=constants.top_p,
-                    frequency_penalty=constants.frequency_penalty,
-                    presence_penalty=constants.presence_penalty
-                )
-                response = beautify_response(response['choices'][0]['text'])
-                return render_template("analysis_response.html", response=response)
+                responses = {}
+                for title, prompt in prompts.items():
+                    response = openai.Completion.create(
+                        model=constants.model,
+                        prompt=f"""
+                        {prompt}: \n {contents}
+                        """,
+                        temperature=constants.temperature,
+                        max_tokens=constants.max_tokens,
+                        top_p=constants.top_p,
+                        frequency_penalty=constants.frequency_penalty,
+                        presence_penalty=constants.presence_penalty
+                    )
+                    response = beautify_response(response['choices'][0]['text'])
+                    responses[title] = response
+                print(responses)
+                return render_template("analysis_response.html", response=responses)
             except Exception as e:
                 return render_template("analysis_response.html", response=e)
         else:
