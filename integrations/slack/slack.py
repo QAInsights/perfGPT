942cce498162df723973b89b41583d57c353cfb1
NaveenKumar Namachivayam ⚡ <catch.nkn@gmail.com>
2023-02-13 11:01:19 -0500

    Add: Slack integration
    Signed-off-by: NaveenKumar Namachivayam ⚡ <catch.nkn@gmail.com>

--- integrations/slack/slack.py
+++ integrations/slack/slack.py
@@ -11,7 +11,7 @@ def send_slack_notifications(msg):
     }
 
     response = requests.post(
-        'https://hooks.slack.com/services/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',
+        'https://hooks.slack.com/services/{{test}}',
         headers=headers,
         json=json_data,
     )
