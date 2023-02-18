8f1db7f28ef1e18fa8b27a7868f9b2d3eba562e6
NaveenKumar Namachivayam ⚡ <catch.nkn@gmail.com>
2023-02-18 18:51:19 -0500

    Fix: DynamoDB tokens
    Signed-off-by: NaveenKumar Namachivayam ⚡ <catch.nkn@gmail.com>

--- secrets_client.py
+++ secrets_client.py
@@ -1,35 +1,41 @@
 import boto3
 from botocore.exceptions import ClientError
 import json
+from sentry_sdk import capture_exception
+import datetime
 
-import constants
 
-
-class Sts:
-    def __init__(self) -> None:
-        self._aws_access_key_id = None
-        self._aws_secret_access_key = None
-        self._aws_session_token = None
-
-    def set_credentials(self, aws_access_key_id, aws_secret_access_key, aws_session_token):
-        self._aws_access_key_id = aws_access_key_id
-        self._aws_secret_access_key = aws_secret_access_key
-        self._aws_session_token = aws_session_token
+class STSCredentials:
+    def __init__(self, role_arn):
+        self.role_arn = role_arn
+        self.session_name = "my_session"
+        self.credentials = None
+        self.expiration = None
 
     def get_credentials(self):
-        return self._aws_access_key_id, self._aws_secret_access_key, self._aws_session_token
+        now_utc = datetime.datetime.utcnow()
+        now_utc_str = now_utc.strftime('%Y-%m-%d %H:%M:%S+00:00')
+        now_utc = datetime.datetime.strptime(now_utc_str, '%Y-%m-%d %H:%M:%S+00:00')
+        if self.expiration:
+            expiration_str = self.expiration.strftime('%Y-%m-%d %H:%M:%S+00:00')
+            expiration = datetime.datetime.strptime(expiration_str, '%Y-%m-%d %H:%M:%S+00:00')
 
-    @property
-    def aws_access_key_id(self):
-        return self._aws_access_key_id
+        if self.credentials is None or now_utc >= expiration:
+            sts_client = boto3.client('sts')
+            response = sts_client.assume_role(
+                RoleArn=self.role_arn,
+                RoleSessionName=self.session_name,
+                DurationSeconds=900
+            )
+            self.credentials = response['Credentials']
+            expiration_str = str(self.credentials['Expiration'])
+            dt = datetime.datetime.fromisoformat(expiration_str)
+            self.expiration = dt - datetime.timedelta(minutes=5)
 
-    @property
-    def aws_secret_access_key(self):
-        return self._aws_secret_access_key
+        return self.credentials
 
-    @property
-    def aws_session_token(self):
-        return self._aws_session_token
+    def set_arn(self, role_arn):
+        self.role_arn = role_arn
 
 
 def get_secret(secret_name, region_name, credentials):
@@ -42,11 +48,12 @@ def get_secret(secret_name, region_name, credentials):
     :return str: value of the secret
     """
     try:
+
         secrets_client = boto3.client(
             'secretsmanager',
-            aws_access_key_id=credentials.aws_access_key_id,
-            aws_secret_access_key=credentials.aws_secret_access_key,
-            aws_session_token=credentials.aws_session_token,
+            aws_access_key_id=credentials['AccessKeyId'],
+            aws_secret_access_key=credentials['SecretAccessKey'],
+            aws_session_token=credentials['SessionToken'],
             region_name=region_name)
 
         get_secret_value_response = secrets_client.get_secret_value(
@@ -54,9 +61,9 @@ def get_secret(secret_name, region_name, credentials):
         )
 
     except ClientError as e:
+        capture_exception(e)
         raise e
 
     secret = json.loads(get_secret_value_response['SecretString'])
 
     return secret[secret_name]
-
